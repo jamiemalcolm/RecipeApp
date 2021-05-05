e2ab2b8747a2a32012113930a6afa784c5eca15c
jamiemalcolm <jmalcolm281@gmail.com>
2021-05-05 17:09:31 +0100

    added showsteps,  and make me button, and changed component name to details

---
+++ client/src/components/RecipeDetails.jsx
@@ -0,0 +1,22 @@
+import { Divider, ListItem } from '@material-ui/core'
+import React from 'react'
+
+const RecipeDetails = ({ recipe }) => {
+
+    const ingredientsNodes = recipe.ingredients.map((ingredient) => {
+        return <div>
+                <ListItem>{ingredient.name}  {ingredient.qty}</ListItem>
+                </div>
+    })
+
+
+return (
+    <div>
+        {ingredientsNodes}
+        
+        
+    </div>
+)
+}
+
+export default RecipeDetails
