965878196c8b0b47bf2e7ccae59492af45622bfe
jamiemalcolm <jmalcolm281@gmail.com>
2021-05-05 17:11:51 +0100

    added showsteps,  and changed export names. added make me button

--- client/src/components/RecipeItem.jsx
+++ client/src/components/RecipeItem.jsx
@@ -1,24 +1,39 @@
-import { List, ListItem, Typography } from '@material-ui/core';
+import { Button, Divider, List, ListItem, Typography } from '@material-ui/core';
 import React, {useState} from 'react'
-import RecipeIngredients from './RecipeIngredients';
+import RecipeDetails from './RecipeDetails';
+import RecipieSteps from './RecipieSteps';
+
 
 const RecipeItem = ({recipe, index}) => {
 
-    const [showIngredients, setShowIngredients] = useState(false);
+    const [showDetails, setShowDetails] = useState(false);
+    const [showSteps, setShowSteps] = useState(false);
 
     const handleRecipeClick = () =>{
         if(recipe.ingredients){
-            setShowIngredients(!showIngredients)
+            setShowDetails(!showDetails);
+        }
+        if(showSteps){
+            setShowSteps(false);
+        }
+    }
+
+    const handleMakeMeButtonClick = () => {
+        if(recipe.steps){
+            setShowSteps(!showSteps);
         }
     }
     return (
         <div>
-            <List>
-                <ListItem button divider onClick={handleRecipeClick}> <Typography variant="subtitle1">{recipe.name}</Typography></ListItem>
-                <List>
-                    {showIngredients && <RecipeIngredients ingredients={recipe.ingredients} index={index} key={index}/> }
-                </List>
+            <List >
+                <ListItem button divider  style={{width: '50%'}}> < Typography onClick={handleRecipeClick} variant="subtitle1">{`  ${recipe.name}  `}</Typography>  { showDetails && <Button color="primary" size="small" style={{marginLeft: 10}} variant="contained" onClick={handleMakeMeButtonClick}> Make Me! 🍳  </Button>} </ListItem>
             </List>
+                <List style={{display:'flex', justifyItems:'space-between'}}>
+                    {showDetails && <RecipeDetails recipe={recipe} index={index} key={index}/>}
+                {/* </List>
+                <List> */}
+                    {showSteps && <RecipieSteps recipe={recipe}/>}
+                </List>
         </div>
     )
 }
