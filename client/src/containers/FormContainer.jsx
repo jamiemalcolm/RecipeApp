2d0973fc1edbffa796fd23685e48ddf87f713344
jamiemalcolm <jmalcolm281@gmail.com>
2021-05-07 11:40:38 +0100

    added recipe form to form container

---
+++ client/src/containers/FormContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddRecipeForm from '../components/AddRecipeForm'
+
+const FormContainer = () => {
+    return (
+        <div>
+            The form page
+
+            <AddRecipeForm/>
+        </div>
+    )
+}
+
+export default FormContainer
