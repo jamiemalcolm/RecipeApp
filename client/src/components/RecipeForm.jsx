8b8dfbfdbae76816f5249f4dac486c90295b6ce4
jamiemalcolm <jmalcolm281@gmail.com>
2021-05-13 13:09:07 +0100

    from working with multiple fields,  need to fix steps array

--- client/src/components/RecipeForm.jsx
+++ client/src/components/RecipeForm.jsx
@@ -1,6 +1,6 @@
 import { Container, TextField, Button } from '@material-ui/core'
-import React, {useState} from 'react'
-import { makeStyles} from '@material-ui/core/styles';
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import RemoveIcon from '@material-ui/icons/Remove';
 import AddIcon from '@material-ui/icons/Add';
@@ -23,6 +23,7 @@ const RecipeForm = () => {
 
     const classes = useStyles()
 
+    const [recipeName, setRecipeName] = useState()
     const [recipe, setRecipe] = useState();
     const [ingredients, setIngredients] = useState([
         {
@@ -30,53 +31,131 @@ const RecipeForm = () => {
             qty: ""
         },
     ])
+    const [steps, setSteps] = useState([{step:""}]);
 
-    const handleChangeInput = (index, event) => {
-        console.log(index, event.target.name)
+    const handleChangeRecipeName = (event) => {
+        setRecipeName(event.target.value);
     }
+
+    const handleChangeIngredient = (index, event) => {
+        const values = [...ingredients];
+        values[index][event.target.name] = event.target.value;
+        setIngredients(values)
+    }
+
+    const handleStepChange = (index, event) => {
+        const values = [...steps];
+        values[index][event.target.name] = event.target.value;
+        setSteps(values);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const recipeToSave = 
+        {
+            name: recipeName,
+            ingredients: ingredients,
+            steps: steps.map((step) => Object.values(step))
+        }
+        setRecipe(recipeToSave)
+        console.log(recipe);
+    }
+
+    const handleAddIngredient = () => {
+        setIngredients([...ingredients, {
+            name: "",
+            qty: ""
+        },])
+    }
+
+    const handleRemoveIngredient = (index) => {
+        const values = [...ingredients];
+        values.splice(index, 1);
+        setIngredients(values);
+    }
+
+    const handleAddStep = () => {
+        setSteps([...steps, {step:""}])
+    }
+
+    const handleRemoveStep = (index) => {
+        const values = [...steps];
+        values.splice(index, 1);
+        setSteps(values);
+    }
+
     return (
-            <Container>
-                <form className={classes.root}>
+        <Container>
+            <form onSubmit={handleSubmit} className={classes.root}>
                 <TextField
-                    label="name"
+                    label="Name"
                     variant="filled"
-                    name="name"/>
+                    name="name"
+                    onChange={(event) => handleChangeRecipeName(event)}
+                    
+                    />
 
                 {ingredients.map((ingredients, index) => (
                     <div key={index}>
                         <TextField
-                            label="ingredient name"
+                            label="Ingredient Name"
                             variant="filled"
-                            name="ingredient name"
-                            value={ingredients.name}
-                            onChange={event => handleChangeInput(index, event)}
-                            />
+                            name="name"
+                            value={ingredients.name.values}
+                            onChange={(event) => handleChangeIngredient(index, event)}
+                        />
                         <TextField
-                            label="ingredient qty"
+                            label="Ingredient Qty"
                             variant="filled"
-                            name="ingredient qty"
-                            value={ingredients.qty}
-                            onChange={event => handleChangeInput(index, event)}
+                            name="qty"
+                            value={ingredients.qty.values}
+                            onChange={(event) => handleChangeIngredient(index, event)}
 
-                            />
-                            <IconButton>
-                                <RemoveIcon/>
-                            </IconButton>
-                            <IconButton>
-                                <AddIcon/>
-                            </IconButton>
+                        />
+                        <IconButton
+                            onClick={() => handleRemoveIngredient(index)}
+                        >
+                            <RemoveIcon />
+                        </IconButton>
+                        <IconButton
+                            onClick={() => handleAddIngredient()}
+                        >
+                            <AddIcon />
+                        </IconButton>
+                    </div>
+                ))}
+                {steps.map((steps, index) => (
+                    <div key={index}>
+                        <TextField
+                            label={`Step ${index + 1}`}
+                            variant="filled"
+                            name="step"
+                            value={steps.step.values}
+                            multiline={true}
+                            onChange={(event) => handleStepChange(index, event)}
+                        />
+                        <IconButton
+                            onClick={() => handleRemoveStep(index)}
+                        >
+                            <RemoveIcon />
+                        </IconButton>
+                        <IconButton
+                            onClick={() => handleAddStep()}
+                        >
+                            <AddIcon />
+                        </IconButton>
                     </div>
                 ))}
 
-                    <Button className={classes.button}
+                <Button className={classes.button}
                     variant="contained"
                     color="primary"
                     type="submit"
-                    >
-                        Save!
+                >
+                    Save!
                     </Button>
-                </form>
-            </Container>
+            </form>
+        </Container>
     )
 }
 
